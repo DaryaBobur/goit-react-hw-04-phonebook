3f3214899ed60102f33b0931b3e60f3580e3e489
Darya <daryabobur@gmail.com>
2022-09-27 20:22:04 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,16 +2,17 @@ import PropTypes from 'prop-types';
 import { nanoid } from 'nanoid';
 import { Contacts } from './ContactsListStyled';
 
-const ContactsList = ({ contacts, removeContact }) =>{
- return (  
-    <Contacts>
-     {contacts.map(({name, number, id}) => 
-       <li key={nanoid()}>
-         <p>- {name}: {number}</p>
-          <button type='button' onClick={() => removeContact(id)}>Delete</button>
-       </li>)}
-    </Contacts>
-        )
+const ContactsList = ({ contacts, removeContact }) => {
+   return (  
+      <Contacts>
+         {contacts.map(({ name, number, id }) => 
+            <li key={nanoid()}>
+               <p>- {name}: {number}</p>
+               <button type='button' onClick={() => removeContact(id)}>Delete</button>
+            </li>)
+         }
+      </Contacts>
+   )
 }
 
 
@@ -20,8 +21,7 @@ ContactsList.propTypes = {
       name: PropTypes.string.isRequired,
       number: PropTypes.string.isRequired,
       id: PropTypes.string.isRequired,
-  }))
-
+   }))
 }
 
 export default ContactsList;
